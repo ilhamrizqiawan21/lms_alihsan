a3e3228c7120431f4b0d4ad070859d7831dfcd02
ILHAM RIZQIAWAN, S.Pd. <ilhamrizqiawan@gmail.com>
2026-08-19 15:32:24 +0700

    chore(phase6): remove unused notification controller import

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Notifikasi;
 use Carbon\Carbon;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
 use Inertia\Inertia;
