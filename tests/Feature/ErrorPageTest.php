4c09222e958767e635456e428d989c1be5a46299
ILHAM RIZQIAWAN, S.Pd. <ilhamrizqiawan@gmail.com>
2026-08-19 13:54:41 +0700

    test: cover explicit forbidden error responses

--- tests/Feature/ErrorPageTest.php
+++ tests/Feature/ErrorPageTest.php
@@ -8,10 +8,20 @@ use Tests\TestCase;
 
 class ErrorPageTest extends TestCase
 {
+    public function test_web_403_uses_forbidden_page(): void
+    {
+        Route::get('/__test-error-403', fn () => abort(403));
+
+        $response = $this->get('/__test-error-403');
+
+        $response->assertStatus(403);
+        $response->assertSee('Akses Ditolak');
+        $response->assertDontSee('Sistem sedang dalam pemeliharaan');
+    }
+
     public function test_web_404_uses_not_found_page_instead_of_maintenance_page(): void
     {
         $response = $this->get('/halaman-yang-tidak-ada');
-
         $response->assertStatus(404);
         $response->assertSee('Halaman tidak ditemukan');
         $response->assertDontSee('sedang dalam perbaikan/pengembangan');
@@ -20,9 +30,7 @@ class ErrorPageTest extends TestCase
     public function test_web_419_uses_session_expired_page(): void
     {
         Route::get('/__test-error-419', fn () => throw new TokenMismatchException);
-
         $response = $this->get('/__test-error-419');
-
         $response->assertStatus(419);
         $response->assertSee('Sesi Anda sudah berakhir');
         $response->assertDontSee('Sistem sedang dalam pemeliharaan');
@@ -31,9 +39,7 @@ class ErrorPageTest extends TestCase
     public function test_web_429_uses_too_many_requests_page(): void
     {
         Route::get('/__test-error-429', fn () => abort(429));
-
         $response = $this->get('/__test-error-429');
-
         $response->assertStatus(429);
         $response->assertSee('Terlalu banyak permintaan');
         $response->assertDontSee('Sistem sedang dalam pemeliharaan');
@@ -42,9 +48,7 @@ class ErrorPageTest extends TestCase
     public function test_web_500_uses_system_error_page(): void
     {
         Route::get('/__test-error-500', fn () => abort(500));
-
         $response = $this->get('/__test-error-500');
-
         $response->assertStatus(500);
         $response->assertSee('Terjadi kesalahan sistem');
         $response->assertDontSee('Sistem sedang dalam pemeliharaan');
@@ -53,9 +57,7 @@ class ErrorPageTest extends TestCase
     public function test_web_503_uses_maintenance_page(): void
     {
         Route::get('/__test-error-503', fn () => abort(503));
-
         $response = $this->get('/__test-error-503');
-
         $response->assertStatus(503);
         $response->assertSee('Sistem sedang dalam pemeliharaan');
         $response->assertDontSee('Terjadi kesalahan sistem');
